c72f6aba9d1773f73ea1f9af82627e759d08dd07
Alexandre C <cechinel88@gmail.com>
2017-03-27 13:18:34 -0300

    solution exercise 3

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # JavaScript-ES6
 
 Exercises with ES6.
+
+> I'm using nodejs version 6.10.1, just run in the terminal:
+* node main.js
+
---
+++ exercises/3/main.js
@@ -0,0 +1,70 @@
+let database = {
+    tables: {},
+    execute(statement) {
+        if (statement.startsWith("create table")) return this.createTable(statement);
+        if (statement.startsWith("insert")) return this.insert(statement);
+    },
+    createTable(statement) {
+        let parsedStatement = statement.match(/create table ([a-z]+) (\(.*\))/),
+            [undefined, tableName, columns] = parsedStatement;
+
+        columns = columns.replace(/(\(|\))/g, "").split(",");
+
+        this.tables[tableName] = {
+            columns: {},
+            data: []
+        };
+
+        for (let column of columns) {
+            let [name, type] = column.trim().split(" ");
+            this.tables[tableName].columns[name] = type;
+        }
+    },
+    insert(statement) {
+        let parsedStatement = statement.match(/insert into ([a-z]+) (\(.*\)) values (\(.*\))/),
+            [undefined, tableName, columns, values] = parsedStatement;
+
+        columns = columns.replace(/(\(|\))/g, "").split(",");
+        values = values.replace(/(\(|\))/g, "").split(",");
+
+        let row = {};
+        for (let i = 0; i < columns.length; i++) {
+            row[columns[i].trim()] = values[i].trim();
+        }
+
+        this.tables[tableName].data.push(row);
+    }
+};
+
+database.execute("create table author (id number, name string, age number, city string, state string, country string)");
+database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
+database.execute("insert into author (id, name, age) values (1, Linus Torvalds, 47)");
+
+console.log(JSON.stringify(database, null, 2));
+// returns 
+// {
+//   "tables": {
+//     "author": {
+//       "columns": {
+//         "id": "number",
+//         "name": "string",
+//         "age": "number",
+//         "city": "string",
+//         "state": "string",
+//         "country": "string"
+//       },
+//       "data": [
+//         {
+//           "id": "1",
+//           "name": "Douglas Crockford",
+//           "age": "62"
+//         },
+//         {
+//           "id": "1",
+//           "name": "Linus Torvalds",
+//           "age": "47"
+//         }
+//       ]
+//     }
+//   }
+// }

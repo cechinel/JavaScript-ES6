25ba0228a6afc259c01ef083664d151be0709649
Alexandre C <cechinel88@gmail.com>
2017-03-31 19:07:14 -0300

    solution exercise 5

---
+++ exercises/5/main.js
@@ -0,0 +1,95 @@
+class Table {
+    constructor() {
+        this.columns = {};
+        this.data = [];
+    }
+}
+
+class Database {
+    constructor(name = "New Database") {
+        this.name = name;
+        this.tables = {};
+    }
+
+    execute(statement) {
+        let [, init] = statement.match(/([a-z]+)/);
+        return this[init](statement);
+    }
+
+    create(statement) {
+        let parsedStatement = statement.match(/create table ([a-z]+) (\(.*\))/),
+            [undefined, tableName, columns] = parsedStatement;
+
+        columns = columns.replace(/(\(|\))/g, "").split(",");
+
+        this.tables[tableName] = new Table();
+
+        for (let column of columns) {
+            let [name, type] = column.trim().split(" ");
+            this.tables[tableName].columns[name] = type;
+        }
+    }
+
+    insert(statement) {
+        let parsedStatement = statement.match(/insert into ([a-z]+) (\(.*\)) values (\(.*\))/),
+            [undefined, tableName, columns, values] = parsedStatement;
+
+        columns = columns.replace(/(\(|\))/g, "").split(",");
+        values = values.replace(/(\(|\))/g, "").split(",");
+
+        let row = {};
+        for (let i = 0; i < columns.length; i++) {
+            row[columns[i].trim()] = values[i].trim();
+        }
+
+        this.tables[tableName].data.push(row);
+    }
+
+    select(statement) {
+        let parsedStatement = statement.match(/select (.*) from (.*)/),
+            [undefined, columns, tableName] = parsedStatement;
+
+        columns = columns.split(",");
+        if (!(tableName in this.tables)) throw `A tabela ${tableName} não existe`;
+
+        var results = [];
+
+        for (let row of this.tables[tableName].data) {
+            var result = {};
+            for (let column of columns) {
+                column = column.trim();
+                if (!(column in this.tables[tableName].columns)) throw `A coluna ${column} não existe`;
+                Object.assign(result, {
+                    [column]: row[column]
+                });
+            }
+            results.push(result);
+        }
+        return results;
+    }
+};
+
+let database = new Database();
+database.execute("create table author (id number, name string, age number, city string, state string, country string)");
+database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
+database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
+database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
+console.log(JSON.stringify(database.execute("select id, name, age from author"), null, 2));
+// returns
+// [
+//   {
+//     "id": "1",
+//     "name": "Douglas Crockford",
+//     "age": "62"
+//   },
+//   {
+//     "id": "2",
+//     "name": "Linus Torvalds",
+//     "age": "47"
+//   },
+//   {
+//     "id": "3",
+//     "name": "Martin Fowler",
+//     "age": "54"
+//   }
+// ]

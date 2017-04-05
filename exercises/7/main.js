3e0609ce07f1940201e8b8948f234443983320f1
Alexandre C <cechinel88@gmail.com>
2017-04-04 22:50:03 -0300

    solution exercise 7

---
+++ exercises/7/main.js
@@ -0,0 +1,11 @@
+import Database from './Database';
+
+let database = new Database();
+database.execute("create table author (id number, name string, age number, city string, state string, country string)");
+database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
+database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
+database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
+console.log(JSON.stringify(database.execute("select id, name, age from author"), null, 2));
+console.log(JSON.stringify(database.execute("select id, name, age from author where id = 1"), null, 2));
+console.log(JSON.stringify(database.execute("select id, name, age from author where age < 60"), null, 2));
+console.log(JSON.stringify(database.execute("select id, name, age from author where name = Linus Torvalds"), null, 2));
